088b618d27cd7bad7992b9a0bcdf3d539ff2af9e
Katerina <kbaneva@gmail.com>
2016-02-21 15:04:54 +0200

    Added function fo updating the sttus of completion, function for changing the name of the list.

--- udacitask/todolist.rb
+++ udacitask/todolist.rb
@@ -5,6 +5,10 @@ class TodoList
         @title = list_title
         @items = Array.new # Starts empty! No Items yet!
      end
+     
+     def update_title_list(list_title)
+        @title = list_title
+     end
 
      # Create a new Item and adds it to the array of Items
      def add_item(new_item)
@@ -16,9 +20,19 @@ class TodoList
         @items.delete_at(item)
      end
 
+     def change_status(index)
+        @items[index].completed_status = "Done"
+     end
+     
+
      def print_list
+        $report_file.write("\n")
+        $report_file.write("#{title}".ljust(56) + "Status")
+        $report_file.write("\n")
+        $report_file.write('#' * 75)
+        $report_file.write("\n")
         @items.each_with_index {|item,index| 
-            $report_file.write("#{index + 1} - #{@items[index].description}" + " " + "Completed: #{@items[index].completed_status}\n")}
+            $report_file.write("#{index + 1} - #{@items[index].description}".ljust(55) + " " + "Completed?: #{@items[index].completed_status}\n")}
      end
     
     
@@ -31,7 +45,7 @@ class Item
     # not complete
     def initialize(item_description)
     	@description = item_description
-    	@completed_status = false
+    	@completed_status = "Not yet"
     end
     
 end
--- udacitask/udacitask.rb
+++ udacitask/udacitask.rb
@@ -1,9 +1,11 @@
 require_relative 'todolist.rb'
 
 #Creating text file to store the information
+
 path = File.join(File.dirname(__FILE__), '../udacitask/todolist.rb')
 file = File.read(path)
 $report_file = File.new("report.txt", "w+")
+
 
 
 # Creates a new todo list
@@ -13,29 +15,32 @@ list = TodoList.new("Things to do everyday")
 # Add four new items
 list.add_item("Yoga")
 list.add_item("Coffee and breakfast")
-list.add_item("Work through the book 'AWP Practical Object oriented Design in Ruby'")
+list.add_item("Work through the book'AWP Practical OOD in Ruby'")
 list.add_item("Work on Udacitask")
 
 # Print the list
-
 list.print_list
-$report_file.write("\n")
 
 # Delete the first item
 list.remove_item(0)
 
 # Print the list
-$report_file.write("\n")
 list.print_list
-$report_file.write("\n")
- 
+
+# Delete the second item
+list.remove_item(0)
 
 # Print the list
+list.print_list
 
 # Update the completion status of the first item to complete
+list.change_status(0)
 
 # Print the list
+list.print_list
 
 # Update the title of the list
+list.update_title_list("My Udacious List")
 
 # Print the list
+list.print_list
