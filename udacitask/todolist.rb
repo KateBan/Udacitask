f675f37425a9f0b076e5020cda970d6c5ef128d5
Katerina <kbaneva@gmail.com>
2016-02-21 13:31:56 +0200

    Added four items, added printing function and function to remove items form the list.

--- udacitask/todolist.rb
+++ udacitask/todolist.rb
@@ -1,7 +1,38 @@
 class TodoList
-    # methods and stuff go here
+	attr_accessor :title, :items
+     # Initialize todo list with a title and no items
+     def initialize(list_title)
+        @title = list_title
+        @items = Array.new # Starts empty! No Items yet!
+     end
+
+     # Create a new Item and adds it to the array of Items
+     def add_item(new_item)
+     	item = Item.new(new_item)
+     	@items.push(item)
+     end
+
+     def remove_item(item)
+        @items.delete_at(item)
+     end
+
+     def print_list
+        @items.each_with_index {|item,index| 
+            $report_file.write("#{index + 1} - #{@items[index].description}" + " " + "Completed: #{@items[index].completed_status}\n")}
+     end
+    
+    
 end
 
 class Item
-    # methods and stuff go here
+    attr_accessor :description, :completed_status
+
+    # Initialize item with a description and marked as 
+    # not complete
+    def initialize(item_description)
+    	@description = item_description
+    	@completed_status = false
+    end
+    
 end
+
--- udacitask/udacitask.rb
+++ udacitask/udacitask.rb
@@ -1,16 +1,34 @@
 require_relative 'todolist.rb'
 
+#Creating text file to store the information
+path = File.join(File.dirname(__FILE__), '../udacitask/todolist.rb')
+file = File.read(path)
+$report_file = File.new("report.txt", "w+")
+
+
 # Creates a new todo list
+list = TodoList.new("Things to do everyday")
+
 
 # Add four new items
+list.add_item("Yoga")
+list.add_item("Coffee and breakfast")
+list.add_item("Work through the book 'AWP Practical Object oriented Design in Ruby'")
+list.add_item("Work on Udacitask")
 
 # Print the list
+
+list.print_list
+$report_file.write("\n")
 
 # Delete the first item
+list.remove_item(0)
 
 # Print the list
-
-# Delete the second item
+$report_file.write("\n")
+list.print_list
+$report_file.write("\n")
+ 
 
 # Print the list
 
