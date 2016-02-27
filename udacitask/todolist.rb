f887553bc6bbbf8ad0d90df5eee60875b2dc1aa7
Katerina <kbaneva@gmail.com>
2016-02-27 13:20:15 +0200

    Removed global variable for writing a file

--- udacitask/todolist.rb
+++ udacitask/todolist.rb
@@ -1,12 +1,16 @@
 #Add class User
 class User
-    @@user_id = 1
-    attr_accessor :name, :user_id
+
+    @@user_id = 0
+
+    attr_reader :user_id, :user_file
+    attr_accessor :name
+ 
     def initialize(user_name)
         @name = user_name
-        $user_name = File.new("#{user_name}_#{@@user_id}.txt", "w+")
         @@user_id += 1
         @user_id = @@user_id
+        @user_file = File.new("#{name}_#{user_id}.txt", "w+")
     end
 
     def add_list(new_list, user_id)
@@ -19,7 +23,10 @@ class User
 end
 
 class TodoList
-	attr_accessor :title, :items, :user_id
+
+    attr_accessor :title
+    attr_reader :items, :user_id, :user_file
+
      # Initialize todo list with a title and no items
      def initialize(list_title, user_id)
         @title = list_title
@@ -33,8 +40,8 @@ class TodoList
 
      # Create a new Item and adds it to the array of Items
      def add_item(new_item)
-     	item = Item.new(new_item)
-     	@items.push(item)
+        item = Item.new(new_item)
+        @items.push(item)
      end
 
      def remove_item(item)
@@ -46,11 +53,11 @@ class TodoList
      end
 
      def print_file(user_name)    
-        $user_name.write("\n#{title}".ljust(58) + "Status\n")
-        $user_name.write('*' * 75)
-        $user_name.write("\n")
+        user_name.user_file.write("\n#{title}".ljust(58) + "Status\n")
+        user_name.user_file.write('*' * 75)
+        user_name.user_file.write("\n")
         @items.each_with_index {|item,index| 
-             $user_name.write("#{index + 1} - #{@items[index].description}".ljust(56) + " " + "Completed: #{@items[index].completed_status}\n")}     
+             user_name.user_file.write("#{index + 1} - #{@items[index].description}".ljust(56) + " " + "Completed: #{@items[index].completed_status}\n")}     
     end
 end
 
@@ -60,12 +67,11 @@ class Item
     # Initialize item with a description and marked as 
     # not complete
     def initialize(item_description)
-    	@description = item_description
-    	@completed_status = "Not yet"
+        @description = item_description
+        @completed_status = "Not yet"
     end
 
     def completed?
         @completed_status == "Done"
     end 
 end
-
